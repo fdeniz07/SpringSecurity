15f6d94f17e874af969a7172f4be167812150480
Fatih Deniz <deniz.fatih@hotmail.de>
2023-05-05 19:06:43 +0200

    Spring Boot Security via JWT - Day 1

--- spring_jwt/src/main/java/com/tpe/security/service/UserDetailsServiceImpl.java
+++ spring_jwt/src/main/java/com/tpe/security/service/UserDetailsServiceImpl.java
@@ -1,7 +1,27 @@
 package com.tpe.security.service;
 
-public class UserDetailsServiceImpl {
+import com.tpe.domain.User;
+import com.tpe.exception.ResourceNotFoundException;
+import com.tpe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByUserName(username).orElseThrow(() ->
+                new ResourceNotFoundException("User not found with username " + username));
 
 
-
+        return UserDetailsImpl.build(user) ;
+    }
 }
