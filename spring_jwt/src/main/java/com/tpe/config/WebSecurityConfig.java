c6cf89d40fab0aab0e1ddbe08115ad2c5a6b9608
Fatih Deniz <deniz.fatih@hotmail.de>
2023-05-05 16:47:08 +0200

    Spring Boot Security via JWT - Day 1

--- spring_jwt/src/main/java/com/tpe/config/WebSecurityConfig.java
+++ spring_jwt/src/main/java/com/tpe/config/WebSecurityConfig.java
@@ -1,6 +1,69 @@
 package com.tpe.config;
 
-public class WebSecurityConfig {
+import com.tpe.security.AuthTokenFilter;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+@AllArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().
+                sessionManagement().
+                sessionCreationPolicy(SessionCreationPolicy.STATELESS). //REST mimaride Session olmaz.
+                and().
+                authorizeRequests().
+                antMatchers("/register", "/login").permitAll().
+                anyRequest().authenticated();
+
+        http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //!!! dün yazdigimiz kod blogunun kisa hali
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean //Metod bazinda DI yapiyor
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    public AuthTokenFilter authTokenFilter() {
+
+        return new AuthTokenFilter();
+    }
+
+    //TODO provider...
 
 }
